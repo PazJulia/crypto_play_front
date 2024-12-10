905e971a04edcada490191976c2c8886b154689f
PazJulia <paz.julia7@gmail.com>
2024-12-09 21:32:42 -0300

    wip

--- scripts.js
+++ scripts.js
@@ -1,154 +1,59 @@
-/*
-  --------------------------------------------------------------------------------------
-  Função para obter a lista existente do servidor via requisição GET
-  --------------------------------------------------------------------------------------
-*/
-const getList = async () => {
-    let url = 'http://127.0.0.1:5000/produtos';
-    fetch(url, {
-        method: 'get',
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            data.produtos.forEach(item => insertList(item.nome, item.quantidade, item.valor))
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+const searchWrapper = document.querySelector('.input-wrapper');
 
-/*
-  --------------------------------------------------------------------------------------
-  Chamada da função para carregamento inicial dos dados
-  --------------------------------------------------------------------------------------
-*/
-getList()
+function buscarMoeda() {
+    let inputCoin = document.getElementById('coinSearch').value;
 
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para colocar um ítem na lista do servidor via requisição POST
-  --------------------------------------------------------------------------------------
-*/
-const postItem = async (inputProduct, inputQuantity, inputPrice) => {
-    const formData = new FormData();
-    formData.append('nome', inputProduct);
-    formData.append('quantidade', inputQuantity);
-    formData.append('valor', inputPrice);
-
-    let url = 'http://127.0.0.1:5000/produto';
-    fetch(url, {
-        method: 'post',
-        body: formData
-    })
-        .then((response) => response.json())
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
-
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para criar um botão close para cada ítem da lista
-  --------------------------------------------------------------------------------------
-*/
-const insertButton = (parent) => {
-    let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    parent.appendChild(span);
-}
-
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para remover um ítem da lista conforme o click no botão close
-  --------------------------------------------------------------------------------------
-*/
-const removeElement = () => {
-    let close = document.getElementsByClassName("close");
-    // var table = document.getElementById('myTable');
-    let i;
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            let div = this.parentElement.parentElement;
-            const nomeItem = div.getElementsByTagName('td')[0].innerHTML
-            if (confirm("Você tem certeza?")) {
-                div.remove()
-                deleteItem(nomeItem)
-                alert("Removido!")
-            }
-        }
-    }
-}
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para remover um ítem da lista do servidor via requisição DELETE
-  --------------------------------------------------------------------------------------
-*/
-const deleteItem = (item) => {
-    console.log(item)
-    let url = 'http://127.0.0.1:5000/produto?nome=' + item;
-    fetch(url, {
-        method: 'delete'
-    })
-        .then((response) => response.json())
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para adicionar um novo ítem com nome, quantidade e valor
-  --------------------------------------------------------------------------------------
-*/
-const newItem = () => {
-    let inputProduct = document.getElementById("newInput").value;
-    let inputQuantity = document.getElementById("newQuantity").value;
-    let inputPrice = document.getElementById("newPrice").value;
-
-    if (inputProduct === '') {
-        alert("Escreva o nome de um item!");
-    } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
-        alert("Quantidade e valor precisam ser números!");
+    if (inputCoin === '') {
+        alert("Preencha a busca");
     } else {
-        insertList(inputProduct, inputQuantity, inputPrice)
-        postItem(inputProduct, inputQuantity, inputPrice).then()
-        alert("Item adicionado!")
+        searchCoin(inputCoin);
     }
 }
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para inserir ítens na lista apresentada
-  --------------------------------------------------------------------------------------
-*/
-const insertList = (nameProduct, quantity, price) => {
-    const item = [nameProduct, quantity, price];
-    const table = document.getElementById('myTable');
-    const row = table.insertRow();
+async function searchCoin(inputCoin) {
+    const params = new URLSearchParams({ nome: inputCoin });
+    const url = `http://127.0.0.1:5000/moeda?${params.toString()}`;
 
-    for (let i = 0; i < item.length; i++) {
-        const cel = row.insertCell(i);
-        cel.textContent = item[i];
+    try {
+        const response = await fetch(url, { method: 'GET' });
+        const data = await response.json();
+        renderResults(data.coins);
+    } catch (error) {
+        console.error('Error:', error);
     }
-    insertButton(row.insertCell(-1))
-    document.getElementById("newInput").value = "";
-    document.getElementById("newQuantity").value = "";
-    document.getElementById("newPrice").value = "";
-
-    removeElement()
 }
 
-/*
-  --------------------------------------------------------------------------------------
-  Função para alterar tema da página
-  --------------------------------------------------------------------------------------
-*/
+function renderResults(results) {
+    const ul = document.querySelector('.results ul');
+
+    // Clear the previous list items
+    ul.innerHTML = '';
+
+    if (!results.length) {
+        searchWrapper.classList.remove('show');
+        alert("Nenhum resultado encontrado")
+        return;
+    }
+
+    results.forEach((item) => {
+        const li = document.createElement('li');
+        li.textContent = item.name;  // Assuming 'name' is the property you want to display
+
+        // Add a click event listener to each list item
+        li.addEventListener('click', () => {
+            handleItemClick(item); // Handle click event and pass the full item
+        });
+
+        ul.appendChild(li);
+    });
+
+    searchWrapper.classList.add('show');
+}
+
+function handleItemClick(item) {
+    alert(`You clicked on: ${item.name}\nSymbol: ${item.symbol}\nDescription: ${item.description}`);
+    // Perform further actions with the `item` object here
+}
 
 // Obtém o botão de troca de tema e o elemento root do documento
 const themeToggleButton = document.getElementById('themeIcon');
